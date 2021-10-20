7c3d935ba5f6d9f76d9f6f7d09959cd59401cff6
sharif <sharif@ajab.sharifulislam.com>
2021-10-20 10:14:10 +0300

    Menu FADE Animations

--- script.js
+++ script.js
@@ -6,10 +6,10 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
-const navigation = document.querySelectorAll('.nav__link');
 const btnScrollTo = document.querySelector('.btn--scroll-to');
 const section1 = document.querySelector('#section--1');
-const nav = document.querySelector('.nav__links');
+const nav = document.querySelector('.nav');
+const navigation = document.querySelector('.nav__links');
 const link = document.querySelectorAll('.nav__link');
 const tabContainer = document.querySelector('.operations__tab-container');
 const tabs = document.querySelectorAll('.operations__tab');
@@ -43,7 +43,7 @@ document.addEventListener('keydown', function (e) {
 ///////////////////////////////////////
 // Smooth SCROLLING  NAVIGATION 
 // After ES6
-nav.addEventListener('click', function (e) {
+navigation.addEventListener('click', function (e) {
   e.preventDefault();
   // Matching strategy
   if (e.target.classList.contains('nav__link') && e.target.getAttribute('href') !== '#') {
@@ -104,3 +104,21 @@ tabContainer.addEventListener('click', function (e) {
   clicked.classList.add('operations__tab--active');
   document.querySelector(`.operations__content--${clicked.dataset.tab}`).classList.add('operations__content--active');
 });
+///////////////////////////////////////
+// Navigation Fade Animations
+const handleHover = function (e) {
+  if (e.target.classList.contains('nav__link')) {
+    const link = e.target;
+    const siblings = e.target.closest('.nav').querySelectorAll('.nav__link');
+    const logo = e.target.closest('.nav').querySelector('img');
+    siblings.forEach(el => {
+      if (el !== link) {
+        el.style.opacity = this;
+      }
+      logo.style.opacity = this;
+    });
+  }
+};
+
+nav.addEventListener('mouseover', handleHover.bind(0.5));
+nav.addEventListener('mouseout', handleHover.bind(1));
